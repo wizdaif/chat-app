c976147a5f3d19502d0fdcd9f71b2f4e770bd6b3
wizdaif <toca12385@gmail.com>
2024-01-24 09:48:10 -0500

    add xd

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
+<script setup lang="ts">
+const chats = ref([
+  {
+    name: 'jayyyz',
+    message: 'Yo',
+    timestamp: 0,
+  },
+  {
+    name: 'j2',
+    message: 'wsp',
+    timestamp: 1,
+  }
+])
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <h1 class="web-title">(e2e) chat</h1>
+
+    <div class="chat-area">
+      <div class="chat-box">
+        <ul class="messages">
+          <li v-for="chat of chats">
+            <p>
+              {{ chat.message  }}
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="chat-input">
+        <input type="text" placeholder="Enter a message" />
+        <button>></button>
+      </div>
+    </div>
   </div>
 </template>
