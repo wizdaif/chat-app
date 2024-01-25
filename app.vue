9be21f96cf45eac44349c532c1bc71bb7558dafe
wizdaif <toca12385@gmail.com>
2024-01-25 09:41:52 -0500

    changes

--- app.vue
+++ app.vue
@@ -11,7 +11,59 @@ const chats = ref([
     timestamp: 1,
   }
 ])
+
+const input = ref("")
+
+function messageSubmit() {
+  console.log(input.value)
+
+  if (input.value.length > 24) {
+    alert('Only 24 Characters')
+
+    input.value = ""
+  }
+}
 </script>
+
+<style lang="css">
+  body {
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #181818;
+  }
+
+  .web-title {
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .chat-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: white;
+  }
+
+  .chat-input {
+    display: flex;
+    float: bottom;
+    margin-top: 30%;
+    bottom: 0;
+    margin-bottom: 0;
+  }
+
+  .chat-input input[type="text"] {
+    height: 2rem;
+    width: 15rem;
+    color: dimgray;
+  }
+
+  .chat-input button {
+    width: 2.5rem;
+  }
+</style>
 
 <template>
   <div class="container">
@@ -19,18 +71,18 @@ const chats = ref([
 
     <div class="chat-area">
       <div class="chat-box">
-        <ul class="messages">
-          <li v-for="chat of chats">
+        <div class="messages">
+          <div v-for="chat of chats">
             <p>
-              {{ chat.message  }}
+              {{ chat.name }}: {{ chat.message  }}
             </p>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-      <div class="chat-input">
-        <input type="text" placeholder="Enter a message" />
-        <button>></button>
-      </div>
+      <form class="chat-input" @submit.stop.prevent="messageSubmit">
+        <input type="text" placeholder="Enter a message" :value="input" @change="e => input = e.target!.value" />
+        <button @click="messageSubmit"></button>
+      </form>
     </div>
   </div>
 </template>
